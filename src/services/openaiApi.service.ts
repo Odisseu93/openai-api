af18848e9fc2236b754cf2756bf0acc531ad4a16
@Odisseu93 - Ulisses Silvério <contato@ulisses.tec.br>
2024-01-07 11:06:04 -0300

    refactior: optimize chatgpt prompt

--- src/services/openaiApi.service.ts
+++ src/services/openaiApi.service.ts
@@ -17,17 +17,17 @@ class OpenAiApiService {
     additionalInformation?: string
   ) {
     const userMessage = `Crie uma descrição para o anúncio de um produto para o meu e-commerce.
-            Este anúncio de ver composto da seguinte forma: 
-            - primeira linha com o título/nome do produto (1 linha);
-        - segundo parágrafo uma descrição falando dos benefícios ao adiquirir o produto (5 linhas);
-        - terceiro parágrafo específicações técnicas (máximo 7 linhas);
-        - por último itens inclusos (máximo 3 linhas).
+    Este anúncio deve ser composto da seguinte forma: 
+    - título/nome do produto (1 linha);
+    - descrição geral do produto (2-3 linhas);
+    - benefícios ao adquirir o produto (1 linha).
+    - Informações adicionais (máximo 2 linhas).
 
-            segue os dados do produto: 
-            - título (nome): ${name};
-        - categoria: ${category};
-        ${additionalInformation ? additionalInformation : ''}
-        `
+    Dados do produto: 
+    - título (nome): ${name};
+    - categoria: ${category};
+    ${additionalInformation ? additionalInformation : ''}
+    `
 
     const stream = await this._openaiClient.chat.completions.create({
       model: 'gpt-3.5-turbo',
